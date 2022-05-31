e7ab2b775c7d7e5dbe3c02a1e4bbb431b0e15de6
zhangsan <zhangsan@itcast.cn>
2022-05-31 17:06:18 +0800

    增加GitDEMO代码和方法中输出内容

--- GitDemo/src/main/java/com/itheima/GitDemo.java
+++ GitDemo/src/main/java/com/itheima/GitDemo.java
@@ -3,10 +3,10 @@ package com.itheima;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("GitDemo");
-
+        System.out.println("------------------");
     }
 
     public void run1(){
-
+        System.out.println("0.0.0000000000000000");
     }
 }
